cbe5c6849800afe75f21925b5c196d04f44d3767
Helden,Laura L.H.A. van <l.vanhelden@student.fontys.nl>
2020-08-11 16:33:42 +0200

    grid set up

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public int x, y;
+    public int size;
+
+    bool occupied;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
